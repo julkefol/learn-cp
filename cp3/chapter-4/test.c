890d47fd2c68785a4282da02792fab9c81801814
jcnafi <github-buhtig.unportraited@slmail.me>
2023-09-02 23:39:01 +0600

    add headers to test.c

--- cp3/chapter-4/test.c
+++ cp3/chapter-4/test.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "selection-sort.h"
 #include "bubble-sort.h"
+#include "insertion-sort.h"
 
 int main()
 {
     int ara[] = {5, 1, 3, 0, 11, 4, -1};
     bubbleSort(ara, 7);
-    //selectionSort(ara, 7);
+    /* selectionSort(ara, 7); */
+    /* insertionSort(ara, 7); */
 
     for (int i = 0; i < 7; i++) {
         printf("%d ", ara[i]);
